e63421ba17ae91a2f61e25757e91e706ff6dfe2f
ademawan <ademawan1210@gmail.com>
2022-04-20 21:34:59 +0700

    config test

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"backend-ewallet/configs"
+	"backend-ewallet/utils"
+	"fmt"
+	"log"
+
+	"github.com/go-playground/validator"
+	"github.com/labstack/echo"
+)
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	return cv.validator.Struct(i)
+}
 
 func main() {
-	fmt.Println("test")
+
+	config := configs.GetConfig()
+
+	db := utils.InitDB(config)
+
+	fmt.Println(db)
+
+	e := echo.New()
+	e.Validator = &CustomValidator{validator: validator.New()}
+
+	log.Fatal(e.Start(fmt.Sprintf(":%d", config.Port)))
+
 }
