80fe2a320ea46d7f16172b72c5e42390b056dbdd
ademawan <ademawan1210@gmail.com>
2022-04-20 22:30:36 +0700

    create F Login authRepo

--- repository/auth/auth.go
+++ repository/auth/auth.go
@@ -1,1 +1,36 @@
 package auth
+
+import (
+	"backend-ewallet/entities"
+	"backend-ewallet/utils"
+
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+type AuthDb struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) *AuthDb {
+	return &AuthDb{
+		db: db,
+	}
+}
+
+func (ad *AuthDb) Login(email, password string) (entities.User, error) {
+	user := entities.User{}
+
+	if err := ad.db.Model(&user).Where("email = ?", email).First(&user).Error; err != nil {
+		return user, errors.New("email not found")
+	}
+
+	match := utils.CheckPasswordHash(password, user.Password)
+
+	if !match {
+		return user, errors.New("incorrect password")
+	}
+
+	return user, nil
+}
