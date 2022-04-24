39c9c0e979e8771b764bd7f4d7e19e32ef0cf5a9
ademawan <ademawan1210@gmail.com>
2022-04-25 01:19:46 +0700

    create F payment

--- delivery/controllers/transaction/transaction.go
+++ delivery/controllers/transaction/transaction.go
@@ -5,9 +5,11 @@ import (
 	"backend-ewallet/entities"
 	"backend-ewallet/middlewares"
 	"backend-ewallet/repository/transaction"
+	"backend-ewallet/utils"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/midtrans/midtrans-go"
 	"github.com/midtrans/midtrans-go/coreapi"
 )
 
@@ -134,5 +136,107 @@ func (ac *TransactionController) GetByID() echo.HandlerFunc {
 		}
 
 		return c.JSON(http.StatusOK, common.ResponseUser(http.StatusOK, "Success get transaction", res))
+	}
+}
+func (cont *TransactionController) CreatePayment() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		booking_uid := c.Param("booking_uid")
+		var payment_method PaymentTypeRequest
+		// user := middlewares.ExtractTokenId(c)
+
+		payment_method.Payment_method = "gopay"
+
+		var result *coreapi.ChargeReq
+
+		res_booking, err := cont.repo.GetById(booking_uid)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, templates.InternalServerError(http.StatusInternalServerError, "Your booking is not found", nil))
+		}
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, templates.InternalServerError(http.StatusInternalServerError, "Your booking is not found", nil))
+		}
+		switch payment_method.Payment_method {
+		case "gopay":
+			result = &coreapi.ChargeReq{
+				PaymentType: coreapi.PaymentTypeGopay,
+
+				TransactionDetails: midtrans.TransactionDetails{
+					OrderID:  booking_uid,
+					GrossAmt: int64(res_booking.Price_total),
+				},
+				Items: &[]midtrans.ItemDetails{
+					{
+						ID:    booking_uid,
+						Name:  res_booking.Name,
+						Price: int64(res_booking.Price),
+						Qty:   int32(res_booking.Days),
+					},
+				},
+			}
+
+		case "shopeepay":
+			result = &coreapi.ChargeReq{
+				PaymentType: coreapi.PaymentTypeShopeepay,
+
+				TransactionDetails: midtrans.TransactionDetails{
+					OrderID:  booking_uid,
+					GrossAmt: int64(res_booking.Price_total),
+				},
+				Items: &[]midtrans.ItemDetails{
+					{
+						ID:    booking_uid,
+						Name:  res_booking.Name,
+						Price: int64(res_booking.Price),
+						Qty:   int32(res_booking.Days),
+					},
+				},
+				CustomerDetails: &midtrans.CustomerDetails{
+					FName: "roger",
+					LName: "san",
+					Email: "dani@gmail.com",
+					Phone: "089876543210",
+				},
+				ShopeePay: &coreapi.ShopeePayDetails{
+					CallbackUrl: "https://plastic-cougar-32.loca.lt/booking/payment/callback",
+				},
+			}
+		case "qris":
+			result = &coreapi.ChargeReq{
+				PaymentType: coreapi.PaymentTypeQris,
+
+				TransactionDetails: midtrans.TransactionDetails{
+					OrderID:  booking_uid,
+					GrossAmt: int64(res_booking.Price_total),
+				},
+				Items: &[]midtrans.ItemDetails{
+					{
+						ID:    booking_uid,
+						Name:  res_booking.Name,
+						Price: int64(res_booking.Price),
+						Qty:   int32(res_booking.Days),
+					},
+				},
+			}
+
+		}
+
+		apiRes, err := utils.CreateTransaction(cont.mt, result)
+		// log.Info(apiRes)
+
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, templates.InternalServerError(http.StatusInternalServerError, "Failed to create payment", nil))
+
+		}
+
+		var data PaymentResponse
+		data.OrderID = apiRes.OrderID
+		data.GrossAmount = apiRes.GrossAmount
+		data.PaymentType = apiRes.PaymentType
+		// for i := range apiRes.Actions {
+		data.Url = /*  append(data.Url,  */ apiRes.Actions[1].URL /* ) */
+		// }
+
+		return c.JSON(http.StatusOK, templates.Success(http.StatusOK, "Success create payment booking", data))
+
 	}
 }
